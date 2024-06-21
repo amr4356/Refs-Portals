ab946358b11b9b1d42c48b7d5f2b4ef03996f538
amr4356 <amrooo73321@gmail.com>
2024-06-21 18:12:02 +0300

    134. Manipulating the DOM via Refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,6 +6,7 @@ export default function Player() {
   const [enteredPlayerName,setEnteredPlayerName]=useState('');
   function handleClick(){
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value="";
   }
 
   return (
