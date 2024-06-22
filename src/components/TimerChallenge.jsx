b7ffb1b862be6cdcd6a730d214de83961fb6b85e
amr4356 <amrooo73321@gmail.com>
2024-06-22 18:34:29 +0300

    139. Adding a modal component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState,useRef } from "react";
+import ResultModel from "./ResultModel";
 export default function TimerChallenge({title,targetTime}){
     const timer=useRef();
     const [timerStarted,setTimerStarted]=useState(false);
@@ -16,20 +17,22 @@ export default function TimerChallenge({title,targetTime}){
         clearTimeout(timer.current);
     }
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {timerExpired && <p>You lost!</p>}
-            <p className="challenge-time">
-                {targetTime} second{targetTime>1 ? 's' : ''}
-            </p>
-            <p>
-                <button onClick={timerStarted ? handleStop : handleStart}>
-                    {timerStarted ? "Stop" : "Start"} Challenge
-                </button>
-            </p>
-            <p className={timerStarted ? "active" : undefined}>
-                {timerStarted ? 'Time is running...' : 'Timer inactive'}
-            </p>
-        </section>
+        <>
+            {timerExpired && <ResultModel targetTime={targetTime} result="lost"  />}
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime>1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? "Stop" : "Start"} Challenge
+                    </button>
+                </p>
+                <p className={timerStarted ? "active" : undefined}>
+                    {timerStarted ? 'Time is running...' : 'Timer inactive'}
+                </p>
+            </section>
+            </>
     );
 }
